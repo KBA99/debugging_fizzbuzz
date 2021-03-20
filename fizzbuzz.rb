76995cb506d2715cfd2cda025eec1791d1609e08
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-03-20 20:20:31 +0000

    Commit 2: Fixed FizzBuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,26 @@
+
 class FizzBuzz
   def play(number)
-    return "fizz" if is_divisible_by(number, 3)
-    return "fizzbuzz" if is_divisible_by(num 15)
-    return "buzz" if is_divisible_by(num 5)
+    return "fizz" if  is_divisible_by(number, 3)
+    return "fizzbuzz" if   is_divisible_by(number, 15)
+    return "buzz" if is_divisible_by(number, 5)
     number
   end
 
-  def is_divisib1e_by(number, divisor)
-    number / divisor == 0
+
+  def is_divisible_by(number, divisor)
+    # p "The number is #{number}"
+    # p "The divisor is #{divisor}"
+    # p "the result is #{number%divisor}"
+    number % divisor == 0
   end
+
 end
 
 
 fizzbuzz = FizzBuzz.new
+
 (1..100).each do |number|
   puts fizzbuzz.play(number)
 end
+
