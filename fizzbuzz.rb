98f2c2b7c480435c31ed4dd8ce541fcacbd013c8
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-03-20 20:19:55 +0000

    Commit 1: Broken FizzBuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+class FizzBuzz
+  def play(number)
+    return "fizz" if is_divisible_by(number, 3)
+    return "fizzbuzz" if is_divisible_by(num 15)
+    return "buzz" if is_divisible_by(num 5)
+    number
+  end
+
+  def is_divisib1e_by(number, divisor)
+    number / divisor == 0
+  end
+end
+
+
+fizzbuzz = FizzBuzz.new
+(1..100).each do |number|
+  puts fizzbuzz.play(number)
+end
